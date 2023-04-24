aeca6557891afe06793ba854338cc7de8b340c87
Aaron Ayerdis Espinoza <aaronaej7@gmail.com>
2023-04-24 11:26:12 -0600

    Initial commit

---
+++ src/FileCabinet/SuiteScripts/ethos/BomPricingSuitelet/SuiteScripts/ethos_suitelet_reagent.js
@@ -0,0 +1,29 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/file', 'N/https', 'N/query', 'N/record', 'N/runtime', 'N/search', 'N/ui/serverWidget'],
+    /**
+ * @param{file} file
+ * @param{https} https
+ * @param{query} query
+ * @param{record} record
+ * @param{runtime} runtime
+ * @param{search} search
+ * @param{serverWidget} serverWidget
+ */
+    (file, https, query, record, runtime, search, serverWidget) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+
+        }
+
+        return {onRequest}
+
+    });
